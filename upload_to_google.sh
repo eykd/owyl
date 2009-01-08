77c3081ffa3e04197c8c54759d351a45d1fbcba3
eykd <eykd@eykd.net>
2009-01-08 16:57:30 +0000

    Added appropriate labels to google uploader.

--- upload_to_google.sh
+++ upload_to_google.sh
@@ -2,6 +2,6 @@ CONFIG=~/.subversion/auth
 SUMMARY=`pwd|sed -E sN/.+/NN`
 PROJECT=Owyl
 LABELS=`pwd|sed -E sN/.+/NN`
-FILES=`ls dist/*`
+FILES="`ls dist/*` Featured Type-Source OpSys-All"
 
 ./googlecode_upload.py --summary=$SUMMARY --project=$PROJECT --labels=$LABELS $FILES
